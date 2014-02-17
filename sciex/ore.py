93b8e74d12f6bef08c73fcc23f8393f202274378
zoli <zoltan.gaspar@gmail.com>
2014-02-17 21:50:25 +0100

    ore mining fixed

--- sciex/ore.py
+++ sciex/ore.py
@@ -1,4 +1,5 @@
 __author__ = 'zgaspar'
+import math
 def checkio(rgprobe):
     rgyxcouldbe = set()
     rgxywewere = [(x, y) for [y, x, r] in rgprobe]
@@ -9,13 +10,13 @@ def checkio(rgprobe):
             all_ok = True
             for probe in rgprobe:
                 y0, x0, r = probe[0], probe[1], probe[2]
-                all_ok = all_ok and abs(r*r - ((x-x0)*(x-x0) + (y-y0)*(y-y0))) <= 1
+                all_ok = all_ok and round( math.sqrt( abs((x-x0)*(x-x0) + (y-y0)*(y-y0)) ) ) == r
             if all_ok:
                 rgyxcouldbe.add((y, x))
-                assert ((x, y) not in rgxywewere), "should not be here"
 
     print(rgyxcouldbe)
     return list(rgyxcouldbe.pop())
 
 if __name__ == '__main__':
-    assert checkio([[7,3,6],[1,2,1]]) == [1,1]
+    assert checkio([[7,3,6],[1,2,1]]) == [1,3]
+    assert checkio([[7,3,6],[1,2,11]]) == [9, 9]
